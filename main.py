26e1842d282c013f85ae1af737255389477e97b5
Jaibarra <jaibarra@tecnoquimicas.com>
2024-12-14 09:22:09 -0500

    primer archivo

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Crear datos con duplicados
+data = {
+    'id': [1, 2, 3, 2, 4, 5, 3],
+    'nombre': ['Ana', 'Juan', 'PedroZ', 'JuanX', 'Lucía', 'Carlos', 'Pedro']
+}
+
+# Crear el DataFrame
+df = pd.DataFrame(data)
+
+# Eliminar duplicados basado en la columna 'id'
+df_sin_duplicados = df.drop_duplicates(subset='id', keep='last')
+
+print(df_sin_duplicados)
